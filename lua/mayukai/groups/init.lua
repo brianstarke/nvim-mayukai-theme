db433a667a53764987846115a441508e2a9eea9f
Brian Starke <brian.starke@gmail.com>
2026-02-12 21:53:06 -0500

    oops

--- lua/mayukai/groups/init.lua
+++ lua/mayukai/groups/init.lua
@@ -8,14 +8,14 @@ function M.get(palette, config)
   local lsp = require("mayukai.groups.lsp")
   local treesitter = require("mayukai.groups.treesitter")
 
-  vim.tbl_extend("groups", groups, base.get(palette, config))
-  vim.tbl_extend("groups", groups, syntax.get(palette, config))
-  vim.tbl_extend("groups", groups, lsp.get(palette, config))
-  vim.tbl_extend("groups", groups, treesitter.get(palette, config))
+  vim.tbl_extend("force", groups, base.get(palette, config))
+  vim.tbl_extend("force", groups, syntax.get(palette, config))
+  vim.tbl_extend("force", groups, lsp.get(palette, config))
+  vim.tbl_extend("force", groups, treesitter.get(palette, config))
 
   local plugins = require("mayukai.groups.plugins")
   local plugin_hl = plugins.get(palette, config)
-  vim.tbl_extend("groups", groups, plugin_hl)
+  vim.tbl_extend("force", groups, plugin_hl)
 
   return groups
 end
